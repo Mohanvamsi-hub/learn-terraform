6879eba7640b2fb87f7d2a5a1a17f8048f3c9db1
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-06-11 09:47:28 +0530

    terraform code

--- roboshop.tf
+++ roboshop.tf
@@ -36,7 +36,7 @@ resource "null_resource" "followupresource" {
       "rm -rf roboshop-shell",
       "git clone https://github.com/Mohanvamsi-hub/roboshop-shell.git",
       "cd roboshop-shell",
-      "sudo bash ${each.value["name"]}.sh  ${lookup(each.value,each.value["password"],"null")}"
+      "sudo bash ${each.value["name"]}.sh  ${lookup(each.value,"password","null")}"
     ]
   }
 }
